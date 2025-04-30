de7b712d7cb03233fba393bb8cae1b3f21d91762
Erik Varela <119821019+ErikVarela@users.noreply.github.com>
2025-04-30 18:10:44 -0300

    Update ContaTerminal.java

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        int numero = 1021;
+        String agencia = "067-8";
+        String nomeCliente = "MARIO ANDRADE";
+        Double saldo = 237.48;
+
+        System.out.println("Por favor, digite o número da Agência!\n");
+        String entradaAgencia = scanner.nextLine();
+
+        if(entradaAgencia.equals(agencia)){
+            System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + " conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
+        }
+
     }
 }
